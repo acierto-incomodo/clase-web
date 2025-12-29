bfe406dfdfb166ef5e117c38281a6cd431c26b9f
acierto-incomodo <97026099+acierto-incomodo@users.noreply.github.com>
2025-12-29 16:38:08 +0100

    Update repository

---
+++ make.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e
+
+# --- Color and Print Functions ---
+C_RESET='\033[0m'
+C_RED='\033[0;31m'
+C_GREEN='\033[0;32m'
+C_YELLOW='\033[0;33m'
+C_CYAN='\033[0;36m'
+
+print_header() {
+    printf "\n${C_CYAN}=== %s ===${C_RESET}\n" "$1"
+}
+
+print_success() {
+    printf "${C_GREEN}[✔] %s${C_RESET}\n" "$1"
+}
+
+print_info() {
+    printf "${C_YELLOW}[i] %s${C_RESET}\n" "$1"
+}
+
+print_error() {
+    printf "${C_RED}[✖] Error: %s${C_RESET}\n" "$1"
+}
+
+print_header "Updating and Publishing Repository"
+
+print_info "Committing and pushing changes to GitHub..."
+git add .
+git commit -m "Update repository"
+git push
+
+print_success "Repository updated and published successfully!"
+print_info "Repository URL: https://github.com/acierto-incomodo/clase-web"
